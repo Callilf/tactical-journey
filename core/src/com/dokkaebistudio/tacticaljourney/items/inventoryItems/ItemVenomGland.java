dd8730b05aff442bcc3770c48354c63b32a16dda
Callil <frederic.beaufort@gmail.com>
2019-03-13 17:35:18 +0100

    [Venom gland] If thrown on an enemy, inflict poison and generate a single poison puddle.

--- core/src/com/dokkaebistudio/tacticaljourney/items/inventoryItems/ItemVenomGland.java
+++ core/src/com/dokkaebistudio/tacticaljourney/items/inventoryItems/ItemVenomGland.java
@@ -4,17 +4,21 @@
 package com.dokkaebistudio.tacticaljourney.items.inventoryItems;
 
 import java.util.List;
+import java.util.Set;
 
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.math.Vector2;
 import com.dokkaebistudio.tacticaljourney.Assets;
 import com.dokkaebistudio.tacticaljourney.components.BlockExplosionComponent;
+import com.dokkaebistudio.tacticaljourney.components.StatusReceiverComponent;
+import com.dokkaebistudio.tacticaljourney.components.StatusReceiverComponent.StatusActionEnum;
 import com.dokkaebistudio.tacticaljourney.components.creep.CreepComponent;
 import com.dokkaebistudio.tacticaljourney.components.player.InventoryComponent;
 import com.dokkaebistudio.tacticaljourney.items.Item;
 import com.dokkaebistudio.tacticaljourney.items.enums.ItemEnum;
 import com.dokkaebistudio.tacticaljourney.room.Room;
 import com.dokkaebistudio.tacticaljourney.room.Tile;
+import com.dokkaebistudio.tacticaljourney.statuses.debuffs.StatusDebuffPoison;
 import com.dokkaebistudio.tacticaljourney.util.Mappers;
 import com.dokkaebistudio.tacticaljourney.util.TileUtil;
 
@@ -55,28 +59,43 @@ public class ItemVenomGland extends Item {
 		if (inventoryComponent != null) {
 			inventoryComponent.remove(item);
 		}
-		
+
+		// Create a puddle of poison on this location
 		room.entityFactory.creepFactory.createPoison(room, thrownPosition, thrower);
-		
-		List<Tile> adjacentTiles = TileUtil.getAdjacentTiles(thrownPosition, room);
-		for (Tile tile : adjacentTiles) {
-			if (tile.isThrowable(thrower)) {
-				boolean canHavePoison = true;
-				Entity creepAlreadyThere = TileUtil.getEntityWithComponentOnTile(tile.getGridPos(), CreepComponent.class, room);
-				if (creepAlreadyThere != null) {
-					// Replace any creep on this tile
-					room.removeEntity(creepAlreadyThere);
-				}
+
+		Set<Entity> entitiesWithComponentOnTile = TileUtil.getEntitiesWithComponentOnTile(thrownPosition, StatusReceiverComponent.class, room);
+		if (entitiesWithComponentOnTile.isEmpty()) {
+			// Nothing on this tile : Add poison puddles in the 4 adjacent tiles
+						
+			List<Tile> adjacentTiles = TileUtil.getAdjacentTiles(thrownPosition, room);
+			for (Tile tile : adjacentTiles) {
+				if (tile.isThrowable(thrower)) {
+					boolean canHavePoison = true;
+					Entity creepAlreadyThere = TileUtil.getEntityWithComponentOnTile(tile.getGridPos(), CreepComponent.class, room);
+					if (creepAlreadyThere != null) {
+						// Replace any creep on this tile
+						room.removeEntity(creepAlreadyThere);
+					}
+					
+					Entity wall = TileUtil.getEntityWithComponentOnTile(tile.getGridPos(), BlockExplosionComponent.class, room);
+					if (wall != null) {
+						canHavePoison = false;
+					}
 				
-				Entity wall = TileUtil.getEntityWithComponentOnTile(tile.getGridPos(), BlockExplosionComponent.class, room);
-				if (wall != null) {
-					canHavePoison = false;
-				}
-			
-				if (canHavePoison) {
-					room.entityFactory.creepFactory.createPoison(room, tile.getGridPos(), thrower);
+					if (canHavePoison) {
+						room.entityFactory.creepFactory.createPoison(room, tile.getGridPos(), thrower);
+					}
 				}
 			}
+			
+		} else {
+			// Entity on this tile, apply poison
+			
+			for (Entity e : entitiesWithComponentOnTile) {
+				StatusReceiverComponent statusReceiverComponent = Mappers.statusReceiverComponent.get(e);
+				statusReceiverComponent.requestAction(StatusActionEnum.RECEIVE_STATUS, new StatusDebuffPoison(3, thrower));
+			}
+			
 		}
 	}
 }
